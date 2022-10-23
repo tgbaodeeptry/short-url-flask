985401413a059f27caddfb5a9e7fe7adaeb1743a
Bao Tran Gia <supersoulbylt@gmail.com>
2022-10-23 10:26:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-from flask import Flask, jsonify, request, render_template, send_from_directory
-from form import ShortenForm
+import os
 import utils
+
+from flask import Flask, jsonify, request, render_template, send_from_directory, redirect
+from form import ShortenForm, AnaForm
 from threading import Thread
 
-
 app = Flask(__name__, template_folder="templates", static_url_path="")
+
 app.config['SECRET_KEY'] = 'tgbaodeeptry'
-app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdWLT0cAAAAAKaoWbrrzYn8NJg5JjlrYRe9mE_-'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdWLT0cAAAAAGnirpAYmn9DDb5CoC8_-rfxBRX_'
-app.config['RECAPTCHA_OPTIONS'] = {'theme': 'black'}
 
 
 @app.route('/<alias>', methods=['GET'])
 def get_shortened(alias):
     if utils.db is not None and alias in utils.db:
-        return render_template("redirect.html", url=utils.db[alias])
+        utils.db[f"c-{alias}"] += 1
 
+        return redirect(utils.db[alias])
     else:
         return render_template("bad_url.html")
 
 
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory("", request.path[1:])
+
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-    form = ShortenForm(csrf_enabled=True)
+    form = ShortenForm()
+    form2 = AnaForm()
 
     if request.method == "POST":
         if form.validate_on_submit():
@@ -37,14 +43,33 @@ def home():
 
             shortened_url = utils.shorten(url)
             utils.db[shortened_url] = url
+            utils.db[f"c-{shortened_url}"] = 0
 
-            return jsonify({'message': f"Your short URL is: <a href='/{shortened_url}' target='_blank'>"
-                                       f"https://su.tgbao.me/{shortened_url}"
-                                       f"</a><p></p>Your long URL is: <a href='{url}'>{url}</a>"}), 201
+            return jsonify({
+                'message':
+                f"Your short URL is: <a href='/{shortened_url}' target='_blank' id='url-shortened'>"
+                f"https://su.xbaotg.com/{shortened_url}"
+                f"</a><br/>Your long URL is: <a href='{url}'>{url}</a>"
+            }), 201
 
-        return jsonify({"message": "Unknown error. Try again"})
+        elif form2.validate_on_submit():
+            url = form2.code.data
+            code = url.split("/")[-1]
 
-    return render_template("index.html", form=form)
+            if len(code) != 6:
+                return utils.bad_request("Your code is not exists")
+
+            if utils.db is not None and f"c-{code}" in utils.db:
+                return jsonify({
+                    "message":
+                    f"Your code is: {code}<br/>Count view is: {utils.db[f'c-{code}']}"
+                }), 201
+            else:
+                return jsonify({"message": "Your link is not exists"}), 201
+
+        return utils.bad_request("Please enter reCaptcha")
+
+    return render_template("index.html", form=form, form2=form2)
 
 
 @app.route('/assets/js/<path:path>')
@@ -63,8 +88,9 @@ def send_img(path):
 
 
 def run():
-    app.run("0.0.0.0", port=5000)
+    app.run("0.0.0.0", port=5000, debug=True)
 
 
-t = Thread(target=run)
-t.start()
+# t = Thread(target=run)
+# t.start()
+run()
